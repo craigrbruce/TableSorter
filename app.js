271d0dc0b64b95747ca8e3e102e41fea3aa4862f
Craig Bruce <craig.bruce@covata.com>
2014-06-10 21:37:39 +1000

    better message

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 angular.module('demoApp', ["columnSorter"])
-    .controller("demoCtrl", ["$scope", function($scope){
+    .controller("demoCtrl", ["$scope", function ($scope) {
         $scope.sortActions = [];
 
-        $scope.getFoobars = function(params){
-          //call your API with the params
-          //which should be (e.g)
+        $scope.getFoobars = function (params) {
+            //call your API with the params
+            //which should be (e.g)
             /*
-            * {
-            *   orderBy : "ASC",
-            *   sortBy : "name"
-            * }
-            * */
+             * {
+             *   orderBy : "ASC",
+             *   sortBy : "name"
+             * }
+             * */
 
             //return myService.getFoobars(params)
-                //.then(success, error);
+            //.then(success, error);
 
-            $scope.sortActions.push({number : $scope.sortActions.length + 1 , message :  ' you are sorting on ' + params.orderBy + " and " + params.sortBy });
+            $scope.sortActions.push({
+                number: $scope.sortActions.length + 1,
+                message: ' you are ordering by ' + params.orderBy + " and sorting on " + params.sortBy
+            });
 
 
         };
